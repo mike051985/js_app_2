0eff8762d4885e8807e46b84ad840da0f425a14b
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-15 15:54:31 +0800

    index.html main.js style.css

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
+// variables
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+
+// Start the game
+function startGame() {
+    renderGame()
 }
 
-console.log(message)
+// Render the game
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+    messageEl.textContent = message
+}
+
+// Add new card
+function newCard() {
+    let card = 7
+    sum += card
+
+    renderGame()
+}
